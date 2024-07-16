2636a363c30e83651186c45156e8c6d7ed3ca4b0
Julie Dujardin <julie@dujardin.pw>
2024-07-16 15:08:00 +0200

    day 5 part 2 bruteforce

--- src/d5/mod.rs
+++ src/d5/mod.rs
@@ -43,6 +43,47 @@ fn seed1(file_path: &str) -> isize {
     min_location
 }
 
+fn seed2_bruteforce(file_path: &str) -> isize {
+    let file = fs::read_to_string(file_path).unwrap();
+    let mut groups = file.split("\n\n");
+
+    // Init seeds
+    let mut seeds = Vec::new();
+    let ranges = numbers_str_to_vec(&groups.next().unwrap()[7..]);
+    for i in 0..ranges.len()/2  {
+        for j in ranges[i*2]..ranges[i]+ranges[i+1]{
+            seeds.push(j);
+        }
+    }
+
+    for group in groups {
+        let mut lines = group.lines();
+        lines.next(); // ignore first line
+        let mut moved: Vec<usize> = Vec::new();
+
+        for range in lines {
+            let numbers = numbers_str_to_vec(range);
+
+            let src_start = numbers[1];
+            let dest_start = numbers[0];
+            let range_len = numbers[2];
+
+            for i in 0..seeds.len() {
+                // If the seed matches the current range & we did not move it during this group, move it
+                if !moved.contains(&i) && seeds[i] >= src_start && seeds[i] <= src_start + range_len
+                {
+                    seeds[i] += dest_start - src_start;
+                    moved.push(i);
+                }
+            }
+        }
+    }
+
+    let min_location = *seeds.iter().min().unwrap();
+    println!("The lowest location number is {min_location}");
+    min_location
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -50,13 +91,12 @@ mod tests {
     #[test]
     fn p1() {
         assert_eq!(seed1("src/d5/input_test1.txt"), 35); // provided test
-        seed1("src/d5/input.txt");
-        // assert_eq!(seed1("src/d4/input.txt"), 27454);
+        assert_eq!(seed1("src/d5/input.txt"), 175622908);
     }
 
-    // #[test]
-    // fn p2() {
-    //     assert_eq!(seed2("src/d4/input_test1.txt"), 30); // provided test
-    //     assert_eq!(seed2("src/d4/input.txt"), 6857330);
-    // }
+    #[test]
+    fn p2_bruteforce() {
+        assert_eq!(seed2_bruteforce("src/d5/input_test1.txt"), 46); // provided test
+        // seed2_bruteforce("src/d5/input.txt");
+    }
 }
