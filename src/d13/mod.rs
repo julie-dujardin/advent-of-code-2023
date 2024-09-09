8f48b2c8932b8f97841d22c54c89f46fdb6ecb4d
Julie Dujardin <julie@dujardin.pw>
2024-09-09 20:03:10 +0200

    day 13: run fmt + clippy

--- src/d13/mod.rs
+++ src/d13/mod.rs
@@ -14,39 +14,67 @@ fn parse_file(file_path: &str) -> Vec<Vec<Vec<char>>> {
     maps
 }
 
-fn get_pos(map: &Vec<Vec<char>>, horizontal: bool, i: usize, j: usize) -> char{
-    if horizontal {map[i][j]} else { map[j][i] }
+fn get_pos(map: &Vec<Vec<char>>, horizontal: bool, i: usize, j: usize) -> char {
+    if horizontal {
+        map[i][j]
+    } else {
+        map[j][i]
+    }
 }
 
-fn get_diff_count_for_lines(map: &Vec<Vec<char>>, horizontal: bool, i1: usize, i2: usize, wanted_diffs: usize) -> usize {
-    let l2_max = if horizontal {map.first().unwrap().len()} else { map.len() };
+fn get_diff_count_for_lines(
+    map: &Vec<Vec<char>>,
+    horizontal: bool,
+    i1: usize,
+    i2: usize,
+    wanted_diffs: usize,
+) -> usize {
+    let l2_max = if horizontal {
+        map.first().unwrap().len()
+    } else {
+        map.len()
+    };
     let mut diff_count = 0;
     for j in 0..l2_max {
-        if get_pos(map, horizontal, i1, j) != get_pos(map, horizontal, i2 , j){
+        if get_pos(map, horizontal, i1, j) != get_pos(map, horizontal, i2, j) {
             diff_count += 1;
             if diff_count > wanted_diffs {
-                return diff_count
+                return diff_count;
             }
         }
     }
     diff_count
 }
 
-fn get_line_count_before_mirror(map: &Vec<Vec<char>>, horizontal: bool, wanted_diffs: usize) -> usize {
-    let l1_max = if horizontal {map.len()} else { map.first().unwrap().len() };
+fn get_line_count_before_mirror(
+    map: &Vec<Vec<char>>,
+    horizontal: bool,
+    wanted_diffs: usize,
+) -> usize {
+    let l1_max = if horizontal {
+        map.len()
+    } else {
+        map.first().unwrap().len()
+    };
 
     for i in 1..l1_max {
-        let mut diff_count = get_diff_count_for_lines(map, horizontal, i, i-1, wanted_diffs);
+        let mut diff_count = get_diff_count_for_lines(map, horizontal, i, i - 1, wanted_diffs);
         if diff_count <= wanted_diffs {
             for check_delta in 1..min(l1_max - i, i) {
-                diff_count += get_diff_count_for_lines(map, horizontal, i + check_delta, i - 1 - check_delta, wanted_diffs);
+                diff_count += get_diff_count_for_lines(
+                    map,
+                    horizontal,
+                    i + check_delta,
+                    i - 1 - check_delta,
+                    wanted_diffs,
+                );
                 if diff_count > wanted_diffs {
-                    break
+                    break;
                 }
             }
         }
         if diff_count == wanted_diffs {
-            return i
+            return i;
         }
     }
 
@@ -58,7 +86,8 @@ fn mirrors(file_path: &str, acceptable_diffs: usize) -> usize {
     let mut sum = 0;
 
     for map in maps {
-        sum += 100*get_line_count_before_mirror(&map, true, acceptable_diffs) + get_line_count_before_mirror(&map, false, acceptable_diffs);
+        sum += 100 * get_line_count_before_mirror(&map, true, acceptable_diffs)
+            + get_line_count_before_mirror(&map, false, acceptable_diffs);
     }
 
     sum
