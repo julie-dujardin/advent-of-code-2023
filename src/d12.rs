f76e179546269ed9cd005b612b764b043a65f07e
Julie Dujardin <julie@dujardin.pw>
2024-09-21 17:35:59 +0200

    day 12: read file

--- src/d12.rs
+++ src/d12.rs
@@ -1,15 +1,21 @@
 use std::fs;
 
-fn parse_file(file_path: &str) -> Vec<Vec<char>> {
+fn parse_file(file_path: &str) -> Vec<(String, Vec<usize>)> {
     let file = fs::read_to_string(file_path).unwrap();
     let mut map = Vec::new();
     for line in file.lines() {
-        map.push(line.chars().collect());
+        let split: Vec<&str> = line.split(" ").collect();
+
+        map.push((
+            split[0].to_string(),
+            split[1].split(",").map(|c| c.parse().unwrap()).collect(),
+        ));
     }
     map
 }
 
 pub fn spring1(file_path: &str) -> usize {
+    let map = parse_file(file_path);
     0
 }
 
@@ -19,12 +25,17 @@ pub fn spring2(file_path: &str) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::load_output::load_results;
 
     // #[test]
     // fn p1() {
-    // let expected_p1 = load_results("d12", "p1");
-    // assert_eq!(spring1("test-data/d12/input_test1.txt"), expected_p1["input_test1"]);
-    // assert_eq!(spring1("test-data/d12/input.txt"), expected_p1["input"]);
+    //     let expected_p1 = load_results("d12", "p1");
+    //     assert_eq!(
+    //         spring1("test-data/d12/input_test1.txt"),
+    //         expected_p1["input_test1"]
+    //     );
+    //     assert_eq!(spring1("test-data/d12/input.txt"), expected_p1["input"]);
     // }
 
     // #[test]
