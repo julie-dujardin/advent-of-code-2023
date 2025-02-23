3cec9ba7a18c643db54523a335f6388235d14855
Julie Dujardin <julie@dujardin.pw>
2025-02-23 18:08:13 +0100

    day 22 part 2

--- src/d22.rs
+++ src/d22.rs
@@ -2,6 +2,7 @@ use std::collections::{HashMap, HashSet};
 use std::fs;
 
 struct Brick {
+    id: usize,
     coords_start: (usize, usize, usize),
     coords_end: (usize, usize, usize),
     above: HashSet<usize>,
@@ -13,7 +14,7 @@ fn parse_file(file_path: &str) -> Vec<Brick> {
 
     let mut bricks = Vec::new();
 
-    for line in file.lines() {
+    for (i, line) in file.lines().enumerate() {
         let (raw_start, raw_end) = line.split_once('~').unwrap();
         let start = raw_start
             .split(',')
@@ -24,6 +25,7 @@ fn parse_file(file_path: &str) -> Vec<Brick> {
             .map(|x| x.parse::<usize>().unwrap())
             .collect::<Vec<usize>>();
         bricks.push(Brick {
+            id: i,
             coords_start: (start[0], start[1], start[2]),
             coords_end: (end[0], end[1], end[2]),
             above: HashSet::new(),
@@ -110,8 +112,6 @@ impl Map {
                                     set.insert(i);
                                     set
                                 });
-
-                            // bricks[under_brick].above.insert(i);
                         }
                     }
                 }
@@ -159,46 +159,47 @@ pub fn bricks1(file_path: &str) -> usize {
 }
 
 fn count_children_rec(
-    node: usize,
-    bricks_children: &HashMap<usize, HashSet<usize>>,
-    cache: &mut HashMap<usize, HashSet<usize>>,
+    node: &Brick,
+    bricks: &Vec<Brick>,
+    dropped_bricks: &mut HashSet<usize>,
 ) -> HashSet<usize> {
-    // Check if result is already in cache
-    if let Some(cached) = cache.get(&node) {
-        return cached.clone();
-    }
-
-    // TODO check if the node has parents that haven't been visited
-
     let mut nodes = HashSet::new();
-    for child in bricks_children.get(&node).unwrap() {
-        nodes.insert(*child);
-        nodes.extend(count_children_rec(*child, bricks_children, cache));
+    for child in node.above.iter() {
+        // If the child is only supported by this brick or brick that have been dropped, drop it
+        let child_brick = bricks.get(*child).unwrap();
+        if child_brick
+            .under
+            .difference(dropped_bricks)
+            .collect::<Vec<&usize>>()
+            .len()
+            == 0
+        {
+            dropped_bricks.insert(*child);
+            nodes.insert(*child);
+            nodes.extend(count_children_rec(child_brick, bricks, dropped_bricks));
+        }
     }
 
-    // Store result in cache before returning
-    cache.insert(node, nodes.clone());
     nodes
 }
 
-// pub fn bricks2(file_path: &str) -> usize {
-//     let (map, bricks) = parse_n_gravity(file_path);
-//
-//     let bricks_ancestors = get_ancestors(&map, &bricks);
-//     // let bricks_children = get_children(map, &bricks);
-//
-//     let mut counts = HashMap::new();
-//     // for (_higher, lower) in bricks_ancestors.iter() {
-//     //     if lower.len() == 1 {
-//     //         let start_node = lower.iter().next().unwrap();
-//     //         counts.entry(start_node).or_insert(
-//     //             count_children_rec(*start_node, &bricks_children, &mut HashMap::new()).len(),
-//     //         );
-//     //     }
-//     // }
-//
-//     counts.values().sum()
-// }
+pub fn bricks2(file_path: &str) -> usize {
+    let (_map, bricks) = parse_n_gravity(file_path);
+
+    let mut counts = HashMap::new();
+    for brick_id in 0..bricks.len() {
+        if bricks[brick_id].under.len() == 1 {
+            let start_node = bricks[brick_id].under.iter().next().unwrap();
+            let mut dropped_bricks = HashSet::new();
+            dropped_bricks.insert(*start_node);
+            counts.entry(*start_node).or_insert(
+                count_children_rec(&bricks[*start_node], &bricks, &mut dropped_bricks).len(),
+            );
+        }
+    }
+
+    counts.values().sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -228,8 +229,8 @@ mod tests {
         check_results("d22", "p1", bricks1);
     }
 
-    // #[test]
-    // fn p2() {
-    //     check_results("d22", "p2", bricks2);
-    // }
+    #[test]
+    fn p2() {
+        check_results("d22", "p2", bricks2);
+    }
 }
